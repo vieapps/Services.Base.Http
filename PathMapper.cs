05a59c64705ce909cabb19e5e3999f730e3454eb
Quynh Nguyen <mr.quynh@gmail.com>
2020-02-21 10:12:04 +0700

    Improvement: add PathMapper

---
+++ PathMapper.cs
@@ -0,0 +1,14 @@
+﻿namespace net.vieapps.Services
+{
+	/// <summary>
+	/// Abstract of all HTTP path mapper for working with HTTP services in the VIEApps NGX
+	/// </summary>
+	public abstract class PathMapper
+	{
+		/// <summary>
+		/// Branches the request pipeline based on matches of the given request path
+		/// </summary>
+		/// <param name="appBuilder"></param>
+		public abstract void Map(Microsoft.AspNetCore.Builder.IApplicationBuilder appBuilder);
+	}
+}
