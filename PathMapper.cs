cbab27f362159f5d8d60c78e44c85dbf77fb710f
Quynh Nguyen <mr.quynh@gmail.com>
2020-02-23 04:51:28 +0700

    (improve): add collection of routers' events

--- PathMapper.cs
+++ PathMapper.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿#region Related components
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 #if !NETCOREAPP2_1
 using Microsoft.Extensions.Hosting;
 #endif
+using WampSharp.V2.Realm;
+#endregion
+
 namespace net.vieapps.Services
 {
 	/// <summary>
@@ -14,13 +20,17 @@ namespace net.vieapps.Services
 		/// </summary>
 		/// <param name="appBuilder">The application builder for processing</param>
 		/// <param name="appLifetime">The application life-time for registering events</param>
+		/// <param name="onIncomingConnectionEstablished">The collection that contains the actions to run when the incoming connection to API Gateway Router is established</param>
+		/// <param name="onOutgoingConnectionEstablished">The collection that contains the actions to run when the outgoing connection to API Gateway Router is established</param>
 		public abstract void Map(
 			Microsoft.AspNetCore.Builder.IApplicationBuilder appBuilder,
 #if !NETCOREAPP2_1
-			IHostApplicationLifetime appLifetime = null
+			IHostApplicationLifetime appLifetime = null,
 #else
-			IApplicationLifetime appLifetime = null
+			IApplicationLifetime appLifetime = null,
 #endif
+			List<Action<object, WampSessionCreatedEventArgs>> onIncomingConnectionEstablished = null,
+			List<Action<object, WampSessionCreatedEventArgs>> onOutgoingConnectionEstablished = null
 		);
 	}
 }
