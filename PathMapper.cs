3dbfc217b51daeeaa510188daa6617605997d20c
Quynh Nguyen <mr.quynh@gmail.com>
2020-02-23 01:59:43 +0700

    (improv): add application life-time to register events

--- PathMapper.cs
+++ PathMapper.cs
@@ -1,14 +1,26 @@
-﻿namespace net.vieapps.Services
+﻿using Microsoft.AspNetCore.Hosting;
+#if !NETCOREAPP2_1
+using Microsoft.Extensions.Hosting;
+#endif
+namespace net.vieapps.Services
 {
 	/// <summary>
-	/// Abstract of all HTTP path mapper for working with HTTP services in the VIEApps NGX
+	/// Abstract for branching the request pipeline of all HTTP services in the VIEApps NGX
 	/// </summary>
 	public abstract class PathMapper
 	{
 		/// <summary>
 		/// Branches the request pipeline based on matches of the given request path
 		/// </summary>
-		/// <param name="appBuilder"></param>
-		public abstract void Map(Microsoft.AspNetCore.Builder.IApplicationBuilder appBuilder);
+		/// <param name="appBuilder">The application builder for processing</param>
+		/// <param name="appLifetime">The application life-time for registering events</param>
+		public abstract void Map(
+			Microsoft.AspNetCore.Builder.IApplicationBuilder appBuilder,
+#if !NETCOREAPP2_1
+			IHostApplicationLifetime appLifetime = null
+#else
+			IApplicationLifetime appLifetime = null
+#endif
+		);
 	}
 }
