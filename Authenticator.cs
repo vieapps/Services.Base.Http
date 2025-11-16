2f9b50be83e51ff9ce30843ebb446793cc5c2564
Quynh Nguyen <mr.quynh@gmail.com>
2025-11-16 16:52:08 +0700

    (improv): session info

--- Authenticator.cs
+++ Authenticator.cs
@@ -32,6 +32,7 @@ namespace net.vieapps.Services
 
 		public async Task Invoke(HttpContext context)
 		{
+			var isDebugLogEnabled = Global.IsDebugLogEnabled || context.ContainsKey("x-logs");
 			if (!context.Request.Method.IsEquals("OPTIONS") && !context.Request.Method.IsEquals("HEAD"))
 				try
 				{
@@ -40,7 +41,7 @@ namespace net.vieapps.Services
 				catch (Exception ex)
 				{
 					var url = context.GetRequestUrl();
-					if (!url.IsEndsWith(".xml") && !url.IsEndsWith(".json"))
+					if (isDebugLogEnabled || (!url.IsEndsWith(".xml") && !url.IsEndsWith(".json") && !url.IsEndsWith(".txt")))
 						await context.WriteLogsAsync("Authentications",
 							$"Cannot authenticate [{context.Request.Method} {url}]" + "\r\n" +
 							$"- WebSocket: {context.WebSockets.IsWebSocketRequest}" + "\r\n" +
@@ -65,10 +66,14 @@ namespace net.vieapps.Services
 			// prepare
 			var session = context.GetSession();
 			var correlationID = context.GetCorrelationID();
+			var isDebugLogEnabled = Global.IsDebugLogEnabled || context.ContainsKey("x-logs");
 
 			// already logged-in
 			if (context.IsAuthenticated())
 			{
+				if (isDebugLogEnabled)
+					await context.WriteLogsAsync("Authentications", $"Use is already logged-in => [{context.User.Identity.Name}]").ConfigureAwait(false);
+
 				if (string.IsNullOrWhiteSpace(session.User.ID) && string.IsNullOrWhiteSpace(session.User.SessionID))
 				{
 					session.User = context.GetUser();
@@ -94,6 +99,9 @@ namespace net.vieapps.Services
 				var authenticateToken = context.GetParameter("x-app-token") ?? context.GetParameter("x-temp-token");
 				if (string.IsNullOrWhiteSpace(authenticateToken) && context.TryGetHeaderParameter("authorization", out authenticateToken))
 				{
+					if (isDebugLogEnabled)
+						await context.WriteLogsAsync("Authentications", $"Prepare authorization token => [{authenticateToken}]").ConfigureAwait(false);
+
 					var isBasicToken = authenticateToken.IsStartsWith("Basic");
 					authenticateToken = isBasicToken || authenticateToken.IsStartsWith("Bearer") || authenticateToken.IsStartsWith("JWT") ? authenticateToken.ToArray(" ").Last() : null;
 					if (authenticateToken != null)
@@ -122,6 +130,8 @@ namespace net.vieapps.Services
 				var isWebSocketRequest = context.WebSockets.IsWebSocketRequest;
 				if (!string.IsNullOrWhiteSpace(authenticateToken))
 				{
+					if (isDebugLogEnabled)
+						await context.WriteLogsAsync("Authentications", $"Authenticate with ticket token => [{authenticateToken}]").ConfigureAwait(false);
 					var expiresAfter = this.AllowOverrideTokenExpires
 						? Int32.TryParse(context.GetParameter("x-app-token-expires"), out var expires) && expires > 0 ? expires : 0
 						: this.TokenExpiresAfter;
@@ -134,7 +144,7 @@ namespace net.vieapps.Services
 				// websocket
 				else if (isWebSocketRequest)
 				{
-					var exception = new InvalidRequestException("Request is invalid (token is required for this websocket).");
+					var exception = new InvalidRequestException("Request is invalid (token is required for this websocket)");
 					if (this.AllowWebSocketLateVerification)
 					{
 						var sessionID = context.GetParameter("x-session-id");
@@ -156,7 +166,7 @@ namespace net.vieapps.Services
 							if (ex is InvalidSessionException || ex is InvalidTokenSignatureException || ex is InvalidTokenException || ex is TokenNotFoundException || ex is TokenExpiredException || ex is TokenRevokedException)
 								throw;
 							else
-								throw new InvalidRequestException("Request is invalid (token is required for this websocket).", ex);
+								throw new InvalidRequestException("Request is invalid (token is required for this websocket)", ex);
 						}
 					}
 					else
