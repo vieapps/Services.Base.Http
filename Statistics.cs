f1670d1d3816224cafd06d9a24d386ae685d03e7
Quynh Nguyen <mr.quynh@gmail.com>
2026-04-15 19:26:19 +0700

    (improv): add more type of requests

--- Statistics.cs
+++ Statistics.cs
@@ -6,6 +6,8 @@ namespace net.vieapps.Services
 	{
 		long _requestsTotal;
 		int _requestsInFlight;
+		long _requestsHttpTotal;
+		int _requestsHttpInFlight;
 		long _cacheL1Hit304;
 		long _cacheL1Hit200;
 		long _cacheL1Miss;
@@ -18,7 +20,9 @@ namespace net.vieapps.Services
 		long _rpcCompleted;
 		long _rpcLatencyTotal;
 		long _rpcMaxLatency;
+
 		long _lastRequestsTotal;
+		long _lastRequestsHttpTotal;
 		long _lastRpcEntered;
 		long _lastRpcCompleted;
 
@@ -31,24 +35,43 @@ namespace net.vieapps.Services
 			return delta > 0 ? delta / elapsed : 0;
 		}
 
-		public void IncreaseRequest()
+		public void IncreaseRequest(bool updateHttp = true)
 		{
 			Interlocked.Increment(ref this._requestsTotal);
 			Interlocked.Increment(ref this._requestsInFlight);
+
+			if (updateHttp)
+			{
+				Interlocked.Increment(ref this._requestsHttpTotal);
+				Interlocked.Increment(ref this._requestsHttpInFlight);
+			}
 		}
 
-		public void DecreaseRequest()
+		public void DecreaseRequest(bool updateHttp = true)
 		{
 			if (Interlocked.Decrement(ref this._requestsInFlight) < 0)
 				Interlocked.Exchange(ref this._requestsInFlight, 0);
+
+			if (updateHttp)
+			{
+				if (Interlocked.Decrement(ref this._requestsHttpInFlight) < 0)
+					Interlocked.Exchange(ref this._requestsHttpInFlight, 0);
+			}
 		}
 
 		public long RequestsTotal => Volatile.Read(ref this._requestsTotal);
 
 		public int RequestsInFlight => Volatile.Read(ref this._requestsInFlight);
 
-		public double GetRequestsRate(double elapsedSeconds)
-			=> this.GetRate(ref this._lastRequestsTotal, this.RequestsTotal, elapsedSeconds);
+		public long RequestsHttpTotal => Volatile.Read(ref this._requestsHttpTotal);
+
+		public int RequestsHttpInFlight => Volatile.Read(ref this._requestsHttpInFlight);
+
+		public double GetRequestsRate(double elapsedSeconds, bool useHttp = false)
+			=> useHttp ? this.GetRequestsHttpRate(elapsedSeconds) : this.GetRate(ref this._lastRequestsTotal, this.RequestsTotal, elapsedSeconds);
+
+		public double GetRequestsHttpRate(double elapsedSeconds)
+			=> this.GetRate(ref this._lastRequestsHttpTotal, this.RequestsHttpTotal, elapsedSeconds);
 
 		public long L1Hit304()
 			=> Interlocked.Increment(ref this._cacheL1Hit304);
@@ -90,22 +113,23 @@ namespace net.vieapps.Services
 
 		public long CacheL2MissCount => Volatile.Read(ref this._cacheL2Miss);
 
-		double GetRatio(long count)
-			=> this.RequestsTotal > 0
-				? count * 100.0 / this.RequestsTotal
-				: 0;
+		double GetRatio(long count, bool useHttp = false)
+		{
+			var total = useHttp ? this.RequestsHttpTotal : this.RequestsTotal;
+			return total > 0 ? count * 100.0 / total : 0;
+		}
 
-		public double GetCacheL1HitRatio()
-			=> this.GetRatio(this.CacheL1HitCount);
+		public double GetCacheL1HitRatio(bool useHttp = true)
+			=> this.GetRatio(this.CacheL1HitCount, useHttp);
 
-		public double GetCacheL1MissRatio()
-			=> this.GetRatio(this.CacheL1MissCount);
+		public double GetCacheL1MissRatio(bool useHttp = true)
+			=> this.GetRatio(this.CacheL1MissCount, useHttp);
 
-		public double GetCacheL2HitRatio()
-			=> this.GetRatio(this.CacheL2HitCount);
+		public double GetCacheL2HitRatio(bool useHttp = true)
+			=> this.GetRatio(this.CacheL2HitCount, useHttp);
 
-		public double GetCacheL2MissRatio()
-			=> this.GetRatio(this.CacheL2MissCount);
+		public double GetCacheL2MissRatio(bool useHttp = true)
+			=> this.GetRatio(this.CacheL2MissCount, useHttp);
 
 		public void RpcEntered()
 		{
